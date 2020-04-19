573e4e8b2bffbc21dd7c97281e0f265fd12ef6a4
nadmenny <63935738+nadmenny@users.noreply.github.com>
2020-04-19 17:07:43 -0400

    Changed the fucking names
    JOE WTF

--- lap_iter.m
+++ lap_iter.m
@@ -1,12 +1,12 @@
 %% Lap Iteration Function
 
-function [V,t] = lap_iter(chg, Vmax, Vmax_entry)
+function [V,t] = lap_iter(chg, V_inst, Vmax_entry)
 
 %Initialize
-n_seg = length(Vmax);
+n_seg = length(V_inst);
 Vmax_entry = Vmax_entry';
-Vmax = Vmax';
-V = Vmax;
+V_inst = V_inst';
+V = V_inst;
 Vi = zeros(n_seg,1);
 loop = 0;
 
@@ -15,10 +15,10 @@ while max(abs(V - Vi)) > chg
     loop = loop + 1;
     
     for i = n_seg:-1:1
-        if Vmax_entry(i) < Vmax(i)
+        if Vmax_entry(i+1) < V_inst(i)
             Vi(i) = Vmax_entry(i);
         else
-            Vi(i) = Vmax(i);
+            Vi(i) = V_inst(i);
         end
     end
         
--- v_inst.m
+++ v_inst.m
@@ -1,3 +1,5 @@
-function v = v_inst(a_tract,seg,v)
-v = sqrt(v.^2 - (2.*seg.*a_tract));
+function V_inst = v_inst(a_tractp,a_tractc,seg,V_inst)
+
+V_inst = sqrt(V_inst.^2 + (2.*seg.*a_tractc));
+
 end
