7f0ce840b0d18b0a547fe447c7e76a3833ff77bd
batteredbroccoli <63936439+batteredbroccoli@users.noreply.github.com>
2020-04-18 23:31:27 -0400

    pwrtrain work - commented
    added additional comments and removed uncessary lines of code.

--- main.m
+++ main.m
@@ -4,8 +4,8 @@
 %% Vehicle Parameters
 final_dr = 3.8; %final drive ratio
 drv_eff = 1; %driveline efficiency
-r_wheel = 0.2286;
-wheel_rpm = 800;
+r_wheel = 0.2286; %radius wheel
+wheel_rpm = 800; %input wheel RPM
 
 %% Powertrain Parameters
 [wp_trq, wc_trq] = emrax_dat(wheel_rpm, final_dr, drv_eff); %peak and cont trq @wheel
