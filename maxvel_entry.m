1745dfa3af013ede44cba40de2775ac8e8b29b07
batteredbroccoli <63936439+batteredbroccoli@users.noreply.github.com>
2020-04-19 13:20:31 -0400

    plot function and vmax cleanup

--- maxvel_entry.m
+++ maxvel_entry.m
@@ -3,6 +3,6 @@
 function [Vmax_entry] = maxvel_entry(Vmax,Cd,A,m,mu,p,g,R,seg)
 Fn = m*g;
 Fs = 0.5.*p.*(Vmax.^2).*A.*Cd + sqrt((mu.*Fn).^2 - (m.^2.*Vmax.^4)./(R.^2));
-V_entry = sqrt((Vmax.^2) + (2.*seg.*Fs)./m);
+Vmax_entry = sqrt((Vmax.^2) + (2.*seg.*Fs)./m);
 
 end
