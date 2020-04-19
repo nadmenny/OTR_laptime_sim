84aadc8d40ac3baa0c186ba24514492ba924aa2f
batteredbroccoli <63936439+batteredbroccoli@users.noreply.github.com>
2020-04-18 23:28:15 -0400

    pwrtrain work
    Torque vs RPM curve fit added, after significant retard episodes.

--- main.m
+++ main.m
@@ -1,2 +1,12 @@
 % % % Lap time simulator for OTR FSAE Electric % % %
 % This is the main function to initiate a sim
+
+%% Vehicle Parameters
+final_dr = 3.8; %final drive ratio
+drv_eff = 1; %driveline efficiency
+r_wheel = 0.2286;
+wheel_rpm = 800;
+
+%% Powertrain Parameters
+[wp_trq, wc_trq] = emrax_dat(wheel_rpm, final_dr, drv_eff); %peak and cont trq @wheel
+
