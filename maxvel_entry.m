5d67d3de8746e30181144f869ff17b1b06113fcc
JoeCaruana <63936787+JoeCaruana@users.noreply.github.com>
2020-04-19 23:16:09 -0400

    Bug fixes and improvements

--- maxvel_entry.m
+++ maxvel_entry.m
@@ -1,8 +1,8 @@
 %% Calculate max entry velocity into each segment
 
-function [Vmax_entry, Fs] = maxvel_entry(Vmax,Cd,A,m,mu,p,g,R,seg)
+function [Fs] = maxvel_entry(Vmax,Cd,A,m,mu,p,g,R,seg)
 Fn = m*g;
 Fs = 0.5.*p.*(Vmax.^2).*A.*Cd + sqrt((mu.*Fn).^2 - (m.^2.*Vmax.^4)./(R.^2));
-Vmax_entry = sqrt((Vmax.^2) + (2.*seg.*Fs)./m);
+%Vmax_entry = sqrt((Vmax.^2) + (2.*seg.*Fs)./m);
 
 end
